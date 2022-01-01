30285fcf714bea0758eec539ee5cfb61a2f5b17a
Matt Haynes <haynesmk@gmail.com>
2022-01-01 13:22:15 -0500

    Added functionality take input from user for blank tile values when blanks are included in the provided word. The updated score now subtracts the blank tile letter scores from the total word score in update_point_totals(). //TODO handle incorrect number of tile values provided by user. In progress

--- scrabble-scorer.py
+++ scrabble-scorer.py
@@ -52,29 +52,14 @@ def get_players():
 
 """
     Player (key) plays their word which is appended to the word list (value) 
-    in player_to_words dict. First, validates the input is valide (all alpha
+    in player_to_words dict. First, validates the input is valid (all alpha
     charachters) with validate_input(), then checks the word is a valid US 
-    English word with PyEnchant (spellcheck object)
+    English word with spell_check()
 """
 def play_word(player):
     word = validate_input(player)
-    #handles if the word is not a valid US English word. If not, recursively run play_word()
-    try:
-        if spellcheck.check(word) == True:
-            #handles KeyError if the list is initially empty
-            try:
-                final_word = word
-                player_to_words[player].append(final_word)
-            except KeyError:
-                player_to_words[player] = []
-                player_to_words[player].append(final_word)
-        else:
-            raise InvalidWordError
-    except InvalidWordError:
-        print()
-        print(f"Invalid word! Must be a valid word from the English dictionary. Try again, {player}.", end="\n\n")
-        play_word(player)
-
+    result = spell_check(word, player)
+    return result
 
 """
     Validates the input provided by the player doesn't contain any numbers
@@ -96,6 +81,82 @@ def validate_input(player):
             continue
         return word
 
+""" 
+    Checks the word is a valid US English word using Pychant ('spellcheck' object). If so append to 
+    player_to_words. If not raise InvalidWordError and recursively run play_word()
+"""
+def spell_check(original_word, player):
+    word_with_blanks = get_blanks(original_word, player)
+    word = list(word_with_blanks.keys())[0]
+    blanks = list(word_with_blanks.values())[0]
+    result = {}
+    try:
+        if spellcheck.check(word) == True:
+            #handles KeyError if the list is initially empty
+            result[word] = score_word(blanks)
+            try:
+                player_to_words[player].append(result)
+            except KeyError:
+                player_to_words[player] = []
+                player_to_words[player].append(result)
+        else:
+            raise InvalidWordError
+    except InvalidWordError:
+        print(f"Invalid word! Must be a valid word from the English dictionary. Try again, {player}.", end="\n\n")
+        play_word(player)
+
+"""
+    Checks if the provided word contains blanks and gets input from the user match the blank tiles with the corresponding letter. Checks if the input provided
+    by the user is valid. Returs a dictionary with one item containing the full word (key) and the blank tile letter values (value)
+"""
+def get_blanks(word, player):
+    if " " in word:
+        result = {}
+        #keeps track of letter position in word
+        word_count = 0
+        #keeps track of letter position in blank_letters
+        blank_count = 0
+        #keeps track of blank tile letters in order they appear
+        blank_letters = ""
+        #create list from word to allow changing letters for blanks
+        letters = list(word)
+        #Checks if each blank letter provided is valid and if the number of letters provided by the user 
+        # equal the amount of blank tiles played
+        while True:
+            while True:
+                try:
+                    blank_letters = input(f"{player}, please enter your blank tile values in the order the appear in the word. No spaces between the letters: ")
+                    for letter in blank_letters:
+                        if not(letter.isalpha()):
+                            raise InvalidInputError
+                        else:
+                            continue
+                    break
+                except InvalidInputError:
+                    print(f"Word can only contain letters. Try again, {player}!", end="\n\n")
+                    continue
+            #adds the blank tile values (letters) to the word 
+            try:
+                for letter in word:
+                    if letter == " ":
+                        letters[word_count] = blank_letters[blank_count]
+                        blank_count += 1
+                    word_count += 1
+                if len(blank_letters) == blank_count:
+                    result_key = ''.join(letters)
+                    result[result_key] = blank_letters
+                    #Result is {'full word with blank values': 'blank letters used'}. This will allow keeping track of the correct score upon
+                    # updating the score
+                    print()
+                    return result
+            except IndexError:
+                print(f"Invalid number of blank tile values provided. Must equal the number of blank tiles used. Try again, {player}!", end="\n\n")
+                continue
+    #If there are no blanks, return the original word: "" (empty string for no blank values)
+    else:
+        print()
+        result = {word: ""}
+        return result
 
 """ 
     Scores point total for the input word. Iterates through each letter 
@@ -117,17 +178,18 @@ def score_word(word):
         Iterates through each item of player_to_words dict.
         For each player (key), inner loop: 
             Iterates through each word from the word list (value) of each 
-            item from the outer loop and adds to player_points. 
+            item from the outer loop and adds to player_points. For each word,
+            it subtracts the values of the letters used for blank tiles
         Updates player_points (value) for the player (key) in 
         player_to_points dict.
 """
 def update_point_totals():
-    for player, words in player_to_words.items():
-        player_points = 0
-        for word in words:
-            player_points += score_word(word)
+    for (player, words) in player_to_words.items():
+        for word in words[-1].keys():
+            #subtracts the values of the letters used for blank tiles
+            player_points = (score_word(word) - words[-1][word])
         try:
-            player_to_points[player] = player_points
+            player_to_points[player] += player_points
         except KeyError:
             player_to_points.update({player: player_points})
 
